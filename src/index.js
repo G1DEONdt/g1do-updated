b5f9f49342b58447ff02854cc11b8d0fdde3a984
G1DEON <165989663+G1DEONdt@users.noreply.github.com>
2024-09-03 21:15:58 +1000

    Updated scripts
    - Added todo functionality and implemented initial steps to render todos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+import { Project } from './scripts/project';
+import { renderTodo } from './scripts/render';
 import { Todo } from './scripts/todo';
 import './style.css';
 
-// const title = prompt("Enter todo title: ");
-// const description = prompt("Enter todo description: ");
-// const duedate = prompt("Enter todo duedate: ");
-// const notes = prompt("Enter todo notes: ");
 
-// const todo = new Todo(1, title, description, duedate, notes);
 
+const addTodoButton = document.querySelector('.add-todo');
+
+const project1 = new Project();
+
+addTodoButton.addEventListener("click", () => {
+    const title = prompt("Enter todo title: ");
+    const description = prompt("Enter todo description: ");
+    const duedate = prompt("Enter todo duedate: ");
+    const notes = prompt("Enter todo notes: ");
+
+    const todo = new Todo(1, title, description, duedate, notes);
+    console.log(todo);
+    project1.addTodo(todo);
+
+    console.log(project1.project);
+
+    renderTodo(project1);
+})
--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,3 +1,37 @@
-export function sayhi() {
-    console.log("HEY!");
+const container = document.querySelector('.todo-container');
+
+export function renderTodo(project) {
+    for (let item in project.project) {
+        // Create Elements
+        const todo = document.createElement('div');
+        const check = document.createElement('button');
+        const content = document.createElement('div');
+        const title = document.createElement('h3');
+        const due = document.createElement('p');
+        const deleteButton = document.createElement('button');
+
+
+        // Add classes
+        todo.classList.add('todo');
+        check.classList.add('check');
+        content.classList.add('todo-content');
+        deleteButton.classList.add('delete');
+
+        // Fill elements
+        title.innerText = project.project[item].name;
+        due.innerText = project.project[item].dueDate;
+
+        console.log(item);
+
+        // Append elements
+        todo.appendChild(check);
+        todo.appendChild(content);
+
+        content.appendChild(title);
+        content.appendChild(due);
+
+        todo.appendChild(deleteButton);
+
+        container.appendChild(todo);
+    }
 }
